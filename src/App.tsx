269fe418c25749d11fee33d063c0b8951205cf37
fishdontexist <ffishdontexist@gmail.com>
2024-01-05 18:56:19 +0330

    first

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { ContactPage } from './ContactPage';
+import { ThankYouPage } from './pages/ThankYouPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Navigate to="contact" />
+    },
+    {
+      path: '/contact',
+      element: <ContactPage />,
+    },
+    {
+      path: '/thank-you/:name',
+      element: <ThankYouPage />,
+    },
+  ]
+);
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
